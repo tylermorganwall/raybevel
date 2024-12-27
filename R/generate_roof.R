7dec979b06571828e57744a981f952c9f7e04580
tylermorganwall <tylermorganwall@users.noreply.github.com>
2024-12-26 23:55:50 -0500

    -Try printing out raymesh and see if basic scene renders

--- R/generate_roof.R
+++ R/generate_roof.R
@@ -306,9 +306,9 @@ generate_roof = function(skeleton, max_height = NA, vertical_offset = 0,
 #'   roof_model = generate_beveled_polygon(skeleton,
 #'                                         bevel_offsets = bevel,
 #'                                         material = material_list(diffuse="purple"))
-#'
-#'   raymesh_model(roof_model, override_material = FALSE) |>
-#'     add_object(scene_base) |>
+#'   print(roof_model)
+#'   #raymesh_model(roof_model, override_material = FALSE) |>
+#'     scene_base |>
 #'     render_scene(lookfrom=c(10,30,20), sample_method = "sobol_blue",
 #'                  width=800,height=800,fov=0,ortho_dimensions=c(10,10))
 #' }
